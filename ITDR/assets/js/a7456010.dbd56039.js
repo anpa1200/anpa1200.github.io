148332ff3a5065aaa8fa4d82db7612c87495b0c5
anpa1200 <your-email@example.com>
2026-06-13 22:46:05 +0300

    feat(itdr): integrate ecosystem navigation and styling

---
+++ ITDR/assets/js/a7456010.dbd56039.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkitdr=globalThis.webpackChunkitdr||[]).push([[1235],{8552(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
